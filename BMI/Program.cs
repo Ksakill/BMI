e297cdeaedb290ae01df68ea9ec3c6e18c07a86e
Ksakill <132750722+Ksakill@users.noreply.github.com>
2023-08-13 13:47:22 +0200

    Add project files.

---
+++ BMI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            double Vægt, Højde;
+
+            Console.Write("Insert Vægt i KG: ");
+            string weightinput = Console.ReadLine();
+            Vægt = Convert.ToDouble(weightinput);
+
+            Console.Write("Insert Højde CM: ");
+            string heightInput = Console.ReadLine();
+            Højde = Convert.ToDouble(heightInput);
+
+
+            double bmi = calckBMI(Vægt, Højde / 100);
+
+            Console.WriteLine($"Your BMI is {bmi:f2} ");
+            Console.ReadLine();
+        }
+        static double calckBMI(double Vægt, double Højde)
+        {
+            return Vægt / (Højde * Højde);
+        }
+
+
+
+    }
+}
