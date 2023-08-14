d9d14a8e146596e16613c44d44ddf4d82baedd5b
Ksakill <132750722+Ksakill@users.noreply.github.com>
2023-08-14 16:41:12 +0200

    BMI Update Program.cs

--- BMI/Program.cs
+++ BMI/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,13 +27,14 @@ namespace BMICalculator
 
             Console.WriteLine($"Your BMI is {bmi:f2} ");
             Console.ReadLine();
+
+            Console.WriteLine("underwight: 16-18,5     norm: 18,5'24     over weight: 24-30     first degree of obisity: 30-35    second degree of obeisty; 35-40      thred degree of obisty: 40-up");
+            
         }
         static double calckBMI(double Vægt, double Højde)
         {
             return Vægt / (Højde * Højde);
+            
         }
-
-
-
     }
 }
